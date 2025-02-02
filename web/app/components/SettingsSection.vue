86db87144c43ba3232464bf2befd50847fa76891
Mashupmark <mashupmark@kabelbw.de>
2025-02-02 11:19:59 +0000

    extract settings section into reusable component

---
+++ web/app/components/SettingsSection.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup generic="TSchema extends ObjectSchema<any, any>">
+import { type ObjectSchema, type InferOutput, safeParser } from "valibot";
+import type { FormSubmitEvent } from "#ui/types";
+
+const props = defineProps<{
+  title: string;
+  description?: string;
+  state: InferOutput<TSchema>;
+  schema: TSchema;
+}>();
+
+const emit = defineEmits<{
+  submit: [data: FormSubmitEvent<InferOutput<TSchema>>];
+}>();
+</script>
+
+<template>
+  <UForm
+    class="flex flex-col gap-2 p-4 border-b border-gray-200 dark:border-gray-800"
+    :schema="safeParser(props.schema)"
+    :state="props.state"
+    @submit="emit('submit', $event)"
+  >
+    <UFormGroup
+      :label="title"
+      :description
+      size="xl"
+      class="pb-2 flex justify-between"
+      :ui="{ label: { base: 'font-semibold' } }"
+    >
+      <UButton label="Save changes" type="submit" color="black" />
+    </UFormGroup>
+
+    <slot />
+  </UForm>
+</template>
---
+++ web/app/components/WifiForm.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import * as v from "valibot";
+import type { FormSubmitEvent } from "#ui/types";
+import SettingsSection from "./SettingsSection.vue";
+
+const toast = useToast();
+
+const schema = v.object({
+  SSID: v.pipe(v.string(), v.trim(), v.nonEmpty("required")),
+  PW: v.optional(v.pipe(v.string(), v.trim())),
+});
+type Schema = v.InferOutput<typeof schema>;
+
+const state = reactive<Schema>({ SSID: "" });
+
+const onSubmit = async (data: FormSubmitEvent<Schema>) => {
+  try {
+    await $fetch("/api/wifi", { method: "PUT", body: data });
+  } catch {
+    toast.add({
+      title: "Error",
+      description: "Failed to update wifi credentials",
+      color: "red",
+    });
+  }
+};
+</script>
+
+<template>
+  <SettingsSection
+    title="Wifi"
+    description="Update the wifi credentials used to connect to the internet"
+    :schema
+    :state
+    @submit="onSubmit"
+  >
+    <UFormGroup label="SSID" name="SSID" required>
+      <UInput v-model="state.SSID" />
+    </UFormGroup>
+    <UFormGroup label="Password" name="PW">
+      <UInput
+        v-model="state.PW"
+        type="password"
+        trailing-icon="i-heroicons-lock-closed"
+      />
+    </UFormGroup>
+  </SettingsSection>
+</template>
