3259bd6a1d9606727717ead1565a0e78364de96e
Mashupmark <mashupmark@kabelbw.de>
2025-02-02 11:20:18 +0000

    add settings section for clock color

---
+++ web/app/components/ColorForm.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import * as v from "valibot";
+import SettingsSection from "./SettingsSection.vue";
+
+const schema = v.object({
+  color: v.pipe(v.string(), v.hexColor()),
+});
+type Schema = v.InferOutput<typeof schema>;
+
+const state = reactive<Schema>({ color: "#ff0000" });
+</script>
+
+<template>
+  <SettingsSection
+    title="Clock Color"
+    description="Set the color the time is displayed in"
+    :schema
+    :state
+  >
+    <UFormGroup label="Color" name="color" required>
+      <UInput
+        v-model="state.color"
+        type="color"
+        variant="none"
+        :padded="false"
+      />
+    </UFormGroup>
+  </SettingsSection>
+</template>
--- web/app/pages/index.vue
+++ web/app/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
+  <ColorForm />
   <WifiForm />
 </template>
