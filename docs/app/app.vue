6f224dcf7e3c9df1e9cba1e6341e499744dcf832
Mashupmark <mashupmark@kabelbw.de>
2025-05-31 12:46:15 +0000

    feat: add basic nuxt app for docs

---
+++ docs/app/app.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+useHead({
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+  ],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+
+    <UApp>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UApp>
+  </div>
+</template>
