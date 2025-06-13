a2a88c41dcf9073736a8020c5e782c4a03a02f88
Mashupmark <mashupmark@kabelbw.de>
2025-06-13 15:19:36 +0000

    fix: only write "Uhr" for full hours

--- include/Wordclock.h
+++ include/Wordclock.h
@@ -127,7 +127,7 @@ bool Wordclock::maskTime(DateTime dt, uint8_t x, uint8_t y)
     if (y == 9 && x >= 4 && x < 7 && hour == 11) // "Elf"
         return true;
 
-    if (y == 9 && x >= 8) // "Uhr"
+    if (y == 9 && x >= 8 && minute == 0) // "Uhr"
         return true;
 
     return false;
